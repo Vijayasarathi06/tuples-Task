4e81e1d4025be7f62d737677d07fb19c52d5e77e
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-09-18 12:37:45 +0530

    Tuples Task

---
+++ tuple Task.py
@@ -0,0 +1,82 @@
+##Task 1
+tuples=(1,2,3,4,5)
+print(tuples)
+##Task 2
+tuples=("sarathi",1,3,4.5,2)
+print(tuples)
+##Task 3
+tuples=(1,2,3,4,5)
+print(tuples[3])
+##Task 5
+a=list(tuples)
+a.insert(2,'sarathi')
+tuples=tuple(a)
+print(tuples)
+##Task 6
+tuples=(2,3,45,2)
+a=str(tuples)
+print(a)
+##Task 7
+tuples=(1,2,3,4,"s",5,6,7,8,"a")
+print(tuples[4],tuples[-4])
+##Task 9
+tuples=(1,2,3,4,3,5,6,)
+a=tuples.count(3)
+print(a)
+##Task 10
+tuples=(1,2,3,4,"s",7,8,2,'a')
+print("s" in tuples)
+print("v" in tuples)
+##Task 11
+tuples=(1,9,7,6,5)
+a=list(tuples)
+a.append(11)
+tuples=tuple(a)
+print(tuples)
+##Task 12
+tuples=(1,2,3,3,4,5,6)
+a=list(tuples)
+a.remove(3)
+tuples=tuple(a)
+print(tuples)
+##Task 13
+tupes=(1,2,3,4,5,6)
+print(tuples[2:4])
+##Task 14
+tuples=(1,2,3,4,5,6)
+print(tuples[1],tuples[4])
+##Task 15
+tuples=(1,2,3,4,5,6,7)
+length=len(tuples)
+print(length)
+##Task 18
+tuples=(1,2,3,4,5,6,7)
+a=list(tuples)
+a.reverse()
+tuples=tuple(a)
+print(tuples)
+##Task 20
+tuples=(100,200,300,400)
+a=str(tuples)
+tuples=tuple(a)
+print(tuples)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
